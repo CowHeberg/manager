e0ccc2023eaf14bdd761b961cdb5babbd503adb1
Unknown <contact@cow-heberg.fr>
2016-12-22 02:01:49 +0100

    Manager Upload
    I've upload the manager

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Maintenance</title>
+		<style>
+	  		body { text-align: center; padding: 150px; }
+ 	  		h1 { font-size: 50px; }
+ 	  		body { font: 20px Helvetica, sans-serif; color: #333; }
+  			article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  			a { color: #dc8100; text-decoration: none; }
+  			a:hover { color: #333; text-decoration: none; }
+		</style>
+		<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//stats.cow-heberg.fr/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//stats.cow-heberg.fr/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+	</head>
+	<body>
+		<article>
+    <h1>Maintenance.</h1>
+    <div>
+        <p>We're sorry but you can <a href="mailto:contact@cow-heberg.fr">mail</a>, see you soon.</p>
+        <p>&mdash; Cow-Heberg Team</p>
+    </div>
+</article>
+	</body>
+</html>
